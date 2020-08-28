1b45e33b63d33466aef77bfc9f6308c90287ee45
lukesherwood <luke.sherwood55@gmail.com>
2020-08-29 11:39:04 +1200

    removed hike lists routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  resources :hikes_lists
   resources :hikes
   resources :lists
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
