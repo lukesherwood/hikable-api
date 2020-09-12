6f6c13bf62a32044fdc396a2440d54147d613fc6
lukesherwood <luke.sherwood55@gmail.com>
2020-09-12 13:58:48 +1200

    preparing database for introduction of devise and JWT

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
     has_many :lists
     has_many :hikes, through: :lists
-    has_secure_password
 end
--- db/migrate/20200828211456_create_users.rb
+++ db/migrate/20200828211456_create_users.rb
@@ -2,8 +2,6 @@ class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :email
-      t.string :password_digest
       t.string :picture
 
       t.timestamps
