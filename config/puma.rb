d5e4ecdf68559745a2305cd75f8b1bbb3664228b
lukesherwood <luke.sherwood55@gmail.com>
2021-03-29 11:22:47 -0400

    fixed issue and reverted aws changes

--- config/puma.rb
+++ config/puma.rb
@@ -19,9 +19,6 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
-bind 'tcp://0.0.0.0:8080'
-pidfile "/var/run/puma/my_app.sock"
-
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.
