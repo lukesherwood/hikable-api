4e930f88ab7f4f42cf03f3827a480d5ba0c37aa7
lukesherwood <luke.sherwood55@gmail.com>
2021-04-30 10:48:03 -0400

    setting up get url for attached images

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -11,6 +11,14 @@ class Api::V1::ReviewsController < ApplicationController
         @hike = Hike.find(params[:hike_id])
         @user = User.find_by(id: review_params[:user_id])
         review = @hike.reviews.build(review_params)
+        binding.pry
+        if params[:review][:images].present?
+          params[:review][:images].each do |image|
+            image_url = { :image => (url_for image) }
+            image.attributes.merge(image_url)
+            review.images.attach(image)
+          end
+        end
         # review.name = @user.username
         # options = { include: [:comments], fields: { comments: {user: [:name]} } }
         if review.save
@@ -23,7 +31,7 @@ class Api::V1::ReviewsController < ApplicationController
       private
 
       def review_params
-        params.require(:review).permit(:content, :rating, :user_id, :hike_id, :name, :privacy, :images)
+        params.require(:review).permit(:content, :rating, :user_id, :hike_id, :name, :privacy, images: [])
       end
 
 end
--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,6 +1,6 @@
 class ReviewSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
-  attributes :id, :content, :rating, :privacy, :username
+  attributes :id, :content, :rating, :privacy, :username, :images
   has_one :user
   has_one :hike
 
@@ -8,7 +8,7 @@ class ReviewSerializer < ActiveModel::Serializer
     object.user.username
   end
 
-  attribute :image do |object|
-    object.get_image_url()
+  def image
+    object.images.service_url if object.images.attached?
   end
 end
